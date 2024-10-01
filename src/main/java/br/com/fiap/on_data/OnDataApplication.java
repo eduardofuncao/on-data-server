4b798821665323cb4f443fc1615631e9b4cf4b24
Eduardo Função <eduardofuncao@hotmail.com>
2024-10-01 15:06:44 -0300

    create gradle, springboot java project with spring initializr

---
+++ src/main/java/br/com/fiap/on_data/OnDataApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.on_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnDataApplication.class, args);
+	}
+
+}
